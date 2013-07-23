fb75160e9b07aa65d7323ec899175b8d4b56c6fc
Mike Lopez <dev@scidentify.info>
2013-07-23 16:37:50 -0400

    return text possibility

--- termprint.py
+++ termprint.py
@@ -6,13 +6,20 @@ keys = {
     "ERROR": "red"
 }
 
-def termprint(logtype, data, return_text=True):
+def termprint(logtype, data, return_text=False):
     """
     Print log output in pretty formats
     """
-    if not logtype in keys:
-        print colored(data, "white", attrs=["bold"])
+    if return_text:
+        if not logtype in keys:
+            s = colored(data, "white", attrs=["bold"])
+        else:
+            s = colored(data, keys.get(logtype, "white"), attrs=['bold'])
+        return s
     else:
-        print colored(data, keys.get(logtype, "white"), attrs=['bold'])
+        if not logtype in keys:
+            print colored(data, "white", attrs=["bold"])
+        else:
+            print colored(data, keys.get(logtype, "white"), attrs=['bold'])
 
 
